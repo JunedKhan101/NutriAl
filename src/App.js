5c718e6f0736eb0e702f1712530d0cfdb39bf160
Khan Sonu <k.sonuc101@gmail.com>
2020-12-27 20:55:15 +0530

    Added some code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import Data from "./Data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [datastate, setData] = useState({});
+    useEffect(() => {}, [datastate]);
 
+    useEffect(() => { getData(); }, []);
+
+    var data;
+    const getData = async () => {
+        const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&ingr=1%20pie%20pizza`);
+        const data = await response.json();
+        setData(data);
+        console.log(data);
+        console.log(datastate);
+    }
+    return (
+        <div className="App">
+            <form className="search-form">
+                <input className="search-bar" type="text" />&nbsp;
+                <button className="search-button" type="submit">Search</button>
+            </form>
+            <Data data={datastate} />
+        </div>
+    );
+}
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Data({data}) {
+    return(
+        <div>
+            <p>
+                {data.calories}
+            </p>
+        </div>
+    );
+}
+export default Data;

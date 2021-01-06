ffaaa409244f34e24ca4e1945661ead7a5e87915
Khan Sonu <k.sonuc101@gmail.com>
2021-01-06 21:07:54 +0530

    Added Simple/Detail View Logic, data isn't shown, not implemented completely

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,20 @@
 import React, {useEffect, useState} from "react";
 import Data from "./Data";
-//import data from "../ExampleObject";
+import data from "../ExampleObject";
 
 function App() {
     const [datastate, setData] = useState({});
     const [search, setSearch] = useState("");
     const [query, setQuery] = useState("1 ounce walnuts");
+    let [button, setButton] = useState(false);
     // useEffect(() => {}, [datastate]);
     useEffect(() => { getData(); }, [query]);
 
     const getData = async () => {
-        const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&ingr=${query}`);
-        const data = await response.json();
+        //const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&ingr=${query}`);
+        //const data = await response.json();
         setData(data);
-        console.log(data);
+        // console.log(data);
     }
     const updateSearch = e => {
         setSearch(e.target.value);
@@ -21,6 +22,14 @@ function App() {
     const getSearch = e => {
         e.preventDefault();
         setQuery(search);
+    }
+    const toggleButtonState = () => setButton(button = !button);
+
+    const renderButton = () => {
+        if (!button)
+            return <button className="btn btn-primary sidebutton" style={sidebutton_style} onClick={toggleButtonState}>Detail View</button>
+        else
+            return <button className="btn btn-primary sidebutton" style={sidebutton_style} onClick={toggleButtonState}>Simple View</button>
     }
     let search_style = {
         display: 'inline-block',
@@ -43,6 +52,12 @@ function App() {
         transform: 'translate(-50%, -50%)',
         width: '70%',
         height: '300px',
+        visibility: 'hidden',
+    }
+    let sidebutton_style = {
+        position: 'relative',
+        left: '90%',
+        bottom: '3px',
     }
     return (
         <div className="App">
@@ -50,7 +65,8 @@ function App() {
                 <input className="form-control" type="text" style={search_style} value={search} onChange={updateSearch} />&nbsp;
                 <input className="btn btn-primary" type="submit" value="search" style={button_style} />
             </form>
-            <Data data={datastate} />
+            {renderButton()}
+            <Data data={datastate} viewstate={button} />
             <div className="space" style={space_style}></div>
         </div>
     );
--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,31 +1,32 @@
 import React from "react";
 import "../css/Datastyle.css";
+import "../css/nutrition-style.css";
 
-function Data({data}) {
+function Data({data, viewstate}) {
     const getHealthLabels = () => {
         if (data.healthLabels.length == 0) {
-            return <p className="card-text">"None"</p>;
+            return <p classNameName="card-text">"None"</p>;
         } else {
             let str = "";
             data.healthLabels.map( (val) => str += (val + ", "));
-            return <p className="card-text">{str}</p>;
+            return <p classNameName="card-text">{str}</p>;
         }
     }
     const getCautions = () => {
         if (data.cautions.length == 0) {
-            return <p className="card-text">None</p>;
+            return <p classNameName="card-text">None</p>;
         }
         else {
             let str = "";
             data.cautions.map( (val) => str += (val + ", "))
-            return <p className="card-text">{str}</p>;
+            return <p classNameName="card-text">{str}</p>;
         }
     }
     const getParsed = () => {
         if (data.ingredients[0].parsed[0]) {
             var dataobj = data.ingredients[0].parsed[0];
             return (
-                <div className="card-text">
+                <div classNameName="card-text">
                     <text>Quantity</text>: {dataobj.quantity}<br />
                     <text>Measure</text>: {dataobj.measure}<br />
                     <text>Food Match</text>: {dataobj.foodMatch}<br />
@@ -33,7 +34,7 @@ function Data({data}) {
                 </div>
             );
         } else {
-            return <p className="card-text">None</p>;
+            return <p classNameName="card-text">None</p>;
         }
     }
     const getObject = (parameter) => {
@@ -46,58 +47,276 @@ function Data({data}) {
 
         return Object.keys(dataobj).map( (obj, i) => {
             return(
-                <p className="card-text">
+                <p classNameName="card-text">
                     <text>{dataobj[obj].label}</text>: {dataobj[obj].quantity}{dataobj[obj].unit}
                 </p>
             );
         });
     }
-    const Test = () => {
+    let container_style = {
+        position: 'relative',
+        left: '28%',
+        top: '100px',
+        width: '40%',
+    }
+    const Main = () => {
         try {
-            return(
-                <div className="card">
-                  <div className="card-header">
-                    {data.ingredients ? data.ingredients[0].text : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Information</h3>
-                    {data.ingredients ? getParsed() : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Calories: {data.calories}</h3>
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Total nutrients Kcal</h3>
-                    {data.totalNutrientsKCal ? getObject("nutrientsKcal") : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Health Labels</h3>
-                    {data.healthLabels ? getHealthLabels() : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Cautions</h3>
-                    {data.cautions ? getCautions() : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Total Nutrients</h3>
-                    {data.totalNutrients ? getObject("nutrients") : "loading..."}
-                  </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Total Daily</h3>
-                    {data.totalNutrients ? getObject("daily") : "loading..."}
-                  </div>
-                </div>
-            );
+            if (viewstate) {
+                return (
+                    <div classNameName="card">
+                      <div classNameName="card-header">
+                        {data.ingredients ? data.ingredients[0].text : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Information</h3>
+                        {data.ingredients ? getParsed() : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Calories: {data.calories}</h3>
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Total nutrients Kcal</h3>
+                        {data.totalNutrientsKCal ? getObject("nutrientsKcal") : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Health Labels</h3>
+                        {data.healthLabels ? getHealthLabels() : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Cautions</h3>
+                        {data.cautions ? getCautions() : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Total Nutrients</h3>
+                        {data.totalNutrients ? getObject("nutrients") : "loading..."}
+                      </div>
+                      <div classNameName="card-body">
+                        <h3 classNameName="card-title">Total Daily</h3>
+                        {data.totalNutrients ? getObject("daily") : "loading..."}
+                      </div>
+                    </div>
+                );
+            }
+            else {
+                return (
+                    <section className="performance-facts" style={container_style}>
+                      <header className="performance-facts__header">
+                        <h1 className="performance-facts__title">Nutrition Facts</h1>
+                        <p>Serving Size 1/2 cup (about 82g)</p>
+                          <p>Serving Per Container 8</p>
+                      </header>
+                      <table className="performance-facts__table">
+                        <thead>
+                          <tr>
+                            <th colspan="3" className="small-info">
+                              Amount Per Serving
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th colspan="2">
+                              <b>Calories</b>
+                              200
+                            </th>
+                            <td>
+                              Calories from Fat
+                              130
+                            </td>
+                          </tr>
+                          <tr className="thick-row">
+                            <td colspan="3" className="small-info">
+                              <b>% Daily Value*</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">
+                              <b>Total Fat</b>
+                              14g
+                            </th>
+                            <td>
+                              <b>22%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell">
+                            </td>
+                            <th>
+                              Saturated Fat
+                              9g
+                            </th>
+                            <td>
+                              <b>22%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell">
+                            </td>
+                            <th>
+                              Trans Fat
+                              0g
+                            </th>
+                            <td>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">
+                              <b>Cholesterol</b>
+                              55mg
+                            </th>
+                            <td>
+                              <b>18%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">
+                              <b>Sodium</b>
+                              40mg
+                            </th>
+                            <td>
+                              <b>2%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">
+                              <b>Total Carbohydrate</b>
+                              17g
+                            </th>
+                            <td>
+                              <b>6%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell">
+                            </td>
+                            <th>
+                              Dietary Fiber
+                              1g
+                            </th>
+                            <td>
+                              <b>4%</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell">
+                            </td>
+                            <th>
+                              Sugars
+                              14g
+                            </th>
+                            <td>
+                            </td>
+                          </tr>
+                          <tr className="thick-end">
+                            <th colspan="2">
+                              <b>Protein</b>
+                              3g
+                            </th>
+                            <td>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <table className="performance-facts__table--grid">
+                        <tbody>
+                          <tr>
+                            <td colspan="2">
+                              Vitamin A
+                              10%
+                            </td>
+                            <td>
+                              Vitamin C
+                              0%
+                            </td>
+                          </tr>
+                          <tr className="thin-end">
+                            <td colspan="2">
+                              Calcium
+                              10%
+                            </td>
+                            <td>
+                              Iron
+                              6%
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <p className="small-info">* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
+
+                      <table className="performance-facts__table--small small-info">
+                        <thead>
+                          <tr>
+                            <td colspan="2"></td>
+                            <th>Calories:</th>
+                            <th>2,000</th>
+                            <th>2,500</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th colspan="2">Total Fat</th>
+                            <td>Less than</td>
+                            <td>65g</td>
+                            <td>80g</td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell"></td>
+                            <th>Saturated Fat</th>
+                            <td>Less than</td>
+                            <td>20g</td>
+                            <td>25g</td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">Cholesterol</th>
+                            <td>Less than</td>
+                            <td>300mg</td>
+                            <td>300 mg</td>
+                          </tr>
+                          <tr>
+                            <th colspan="2">Sodium</th>
+                            <td>Less than</td>
+                            <td>2,400mg</td>
+                            <td>2,400mg</td>
+                          </tr>
+                          <tr>
+                            <th colspan="3">Total Carbohydrate</th>
+                            <td>300g</td>
+                            <td>375g</td>
+                          </tr>
+                          <tr>
+                            <td className="blank-cell"></td>
+                            <th colspan="2">Dietary Fiber</th>
+                            <td>25g</td>
+                            <td>30g</td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <p className="small-info">
+                        Calories per gram:
+                      </p>
+                      <p className="small-info text-center">
+                        Fat 9
+                        &bull;
+                        Carbohydrate 4
+                        &bull;
+                        Protein 4
+                      </p>
+                    </section>
+                );
+            }
         }
         catch (e) {
-            return(
-                <div className="card">
-                  <div className="card-header">
+            return (
+                <div classNameName="card">
+                  <div classNameName="card-header">
                     An Error Occured
                   </div>
-                  <div className="card-body">
-                    <h3 className="card-title">Error</h3>
-                    Sorry I cannot understand want you mean here are couple of things you can try:<br />
+                  <div classNameName="card-body">
+                    <h3 classNameName="card-title">Error</h3>
+                    Sorry I cannot understand what you mean here are couple of things you can try:<br />
                     1. Always add a quantity eg. 1 Apple<br />
                     2. Try to add a unit or measure eg. 1 cup coffee or 1 dozen banana<br />
                   </div>
@@ -107,7 +326,7 @@ function Data({data}) {
     }
     return(
         <div>
-        {Test()}
+        {Main()}
         </div>
     );
 }

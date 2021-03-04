a9f2a527b6fb14015179b9910fc84475439ea043
JunedKhan101 <k.sonuc101@gmail.com>
2021-03-05 00:08:28 +0530

    Added Graph page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,47 +1,23 @@
-import React, {useEffect, useState} from "react";
-import Data from "./Data";
-import data from "../ExampleObject";
+import React from "react";
+import Main from "./Main";
+import Graph from "./Graph";
 import "../css/index.css";
-require('dotenv').config()
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-    const [datastate, setData] = useState({});
-    const [search, setSearch] = useState("");
-    const [query, setQuery] = useState("1 ounce walnuts");
-    let [button, setButton] = useState(false);
-    useEffect(() => { getData(); }, [query]);
-
-    const getData = async () => {
-        const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}&ingr=${query}`);
-        const data = await response.json();
-        setData(data);
-        // console.log(data);  to see the data in the console
-    }
-    const updateSearch = e => {
-        setSearch(e.target.value);
-    }
-    const getSearch = e => {
-        e.preventDefault();
-        setQuery(search);
-    }
-    const toggleButtonState = () => setButton(button = !button);
-
-    const renderButton = () => {
-        if (!button)
-            return <button className="btn btn-primary viewbutton" onClick={toggleButtonState}>Detail View</button>
-        else
-            return <button className="btn btn-primary viewbutton" onClick={toggleButtonState}>Simple View</button>
-    }
+export default function App() {
     return (
-        <div className="App">
-            <form className="form-group" onSubmit={getSearch}>
-                <input className="form-control search-textinput" type="text" value={search} onChange={updateSearch} />&nbsp;
-                <input className="btn btn-primary search-button" type="submit" value="search" />
-            </form>
-            {renderButton()}
-            <Data data={datastate} viewstate={button} />
-            <div className="space"></div>
-        </div>
+      <div className="App">
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/graph" component={Graph} />
+            </Switch>
+        </Router>
+      </div>
     );
 }
-export default App;
---
+++ src/Components/Graph.js
@@ -0,0 +1,188 @@
+import react, { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { Bar } from 'react-chartjs-2';
+import zoom from 'chartjs-plugin-zoom';
+import "../css/graph.css";
+
+export default function Graph() {
+  const location = useLocation();
+  const data = location.state.datastate ;
+    var basicdata = {
+      labels: ["Calories", "Energy(kcal)", "Total Fat(g)", "Saturated Fat(g)", "Cholestrol(mg)", 
+      "total carbohydrate(g)", "Dietry Fieber(g)", "Sugars(g)", "Protein(g)"],
+      datasets: [
+        {
+          label: data.ingredients[0].text,
+          data: [data.calories, data.totalNutrients.ENERC_KCAL.quantity,
+            data.totalNutrients.FAT.quantity, data.totalNutrients.FASAT.quantity, 
+            data.totalNutrients.CHOLE.quantity, data.totalNutrients.CHOCDF.quantity,
+            data.totalNutrients.FIBTG.quantity, data.totalNutrients.SUGAR.quantity,
+            data.totalNutrients.PROCNT.quantity],
+          backgroundColor: [
+            "rgba(255, 215, 0, 0.4)",
+            "rgba(60, 179, 113, 0.4)",
+            "rgba(255, 0, 0, 0.4)",
+            "rgba(25, 25, 112, 0.4)",
+            "rgba(127, 255, 0, 0.4)",
+            "rgba(233, 150, 122, 0.4)",
+            "rgba(0, 0, 255, 0.4)",
+            "rgba(255, 20, 147, 0.4)",
+            "rgba(135, 206, 250, 0.4)"
+
+          ],
+          borderColor: [
+            "rgba(255, 215, 0, 1)",
+            "rgba(60, 179, 113)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(25, 25, 112, 1)",
+            "rgba(127, 255, 0, 1)",
+            "rgba(233, 150, 122, 1)",
+            "rgba(0, 0, 255, 1)",
+            "rgba(255, 20, 147, 1)",
+            "rgba(135, 206, 250, 1)"
+
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    var fatdata = {
+      labels: ["Total Fat(g)", "Saturated(g)", "Monosaturated(g)", "Polyunsaturated(g)"],
+      datasets: [
+        {
+          label: `Fats in ${data.ingredients[0].text}`,
+          data: [
+            data.totalNutrients.FAT.quantity, data.totalNutrients.FASAT.quantity, 
+            data.totalNutrients.FAMS.quantity, data.totalNutrients.FAPU.quantity,
+          ],
+          backgroundColor: [
+            "rgba(255, 0, 0, 0.4)",
+            "rgba(0, 255, 0, 0.4)",
+            "rgba(0, 0, 255, 0.4)",
+            "rgba(135, 206, 250, 0.4)",
+          ],
+          borderColor: [
+            "rgba(255, 0, 0, 1)",
+            "rgba(0, 255, 0, 1)",
+            "rgba(0, 0, 255, 1)",
+            "rgba(135, 206, 250, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    var otherdata = {
+      labels: ["Sodium(mg)", "Calcium(mg)", "Magnesium(mg)", "Potassium(mg)", "Iron(mg)", 
+      "Zinc(mg)", "Phosphorus(mg)", "Vitamin A(µg)", "Vitamin C(mg)", "Vitamin B6(mg)",
+      "Vitamin B12(µg)", "Vitamin D(µg)", "Vitamin E(mg)", "Vitamin K(µg)", "Folic acid(µg)"],
+      datasets: [
+        {
+          label: `Nutrients in ${data.ingredients[0].text}`,
+          data: [
+            data.totalNutrients.NA.quantity, data.totalNutrients.CA.quantity,
+            data.totalNutrients.MG.quantity, data.totalNutrients.K.quantity, 
+            data.totalNutrients.FE.quantity, data.totalNutrients.ZN.quantity,
+            data.totalNutrients.P.quantity, data.totalNutrients.VITA_RAE.quantity,
+            data.totalNutrients.VITC.quantity, data.totalNutrients.VITB6A.quantity,
+            data.totalNutrients.VITB12.quantity, data.totalNutrients.VITD.quantity,
+            data.totalNutrients.TOCPHA.quantity, data.totalNutrients.VITK1.quantity,
+            data.totalNutrients.FOLAC.quantity],
+          backgroundColor: [
+            "rgba(47, 79, 79, 0.4)",
+            "rgba(107, 142, 35, 0.4)",
+            "rgba(160, 82, 45, 0.4)",
+            "rgba(75, 0, 130, 0.4)",
+            "rgba(255, 0, 0, 0.4)",
+            "rgba(0, 206, 209, 0.4)",
+            "rgba(255, 165, 0, 0.4)",
+            "rgba(255, 255, 0, 0.4)",
+            "rgba(0, 255, 0, 0.4)",
+            "rgba(0, 250, 154, 0.4)",
+            "rgba(0, 0, 255, 0.4)",
+            "rgba(255, 0, 255, 0.4)",
+            "rgba(221, 160, 221, 0.4)",
+            "rgba(255, 239, 213, 0.4)",
+            "rgba(35, 171, 234, 0.4)"
+
+          ],
+          borderColor: [
+            "rgba(47, 79, 79, 1)",
+            "rgba(107, 142, 35, 1)",
+            "rgba(160, 82, 45, 1)",
+            "rgba(75, 0, 130, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(0, 206, 209, 1)",
+            "rgba(255, 165, 0, 1)",
+            "rgba(255, 255, 0, 1)",
+            "rgba(0, 255, 0, 1)",
+            "rgba(0, 250, 154, 1)",
+            "rgba(0, 0, 255, 1)",
+            "rgba(255, 0, 255, 1)",
+            "rgba(221, 160, 221, 1)",
+            "rgba(255, 239, 213, 1)",
+            "rgba(35, 171, 234, 1)"
+
+          ],
+        },
+      ],
+    };
+    var options = {
+      maintainAspectRatio: true,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      plugins: {
+            zoom: {
+                // Container for pan options
+                pan: {
+                    // Boolean to enable panning
+                    enabled: true,
+
+                    // Panning directions. Remove the appropriate direction to disable 
+                    // Eg. 'y' would only allow panning in the y direction
+                    mode: 'xy'
+                },
+
+                // Container for zoom options
+                zoom: {
+                    // Boolean to enable zooming
+                    enabled: true,
+
+                    // Zooming directions. Remove the appropriate direction to disable 
+                    // Eg. 'y' would only allow zooming in the y direction
+                    mode: 'xy',
+                }
+            }
+        }
+    };
+  const mystyle = {
+    width: "900px",
+    height: "600px"
+  }
+  const otherstyle = {
+    width: "1000px",
+    height: "600px" 
+  }
+  return (
+    <div className="chart">
+      <h1>Basic Information</h1>
+      <div className="basic-graph" style={mystyle}>
+        <Bar data={basicdata} options={options} />
+      </div>
+      <h1>Fats</h1>
+      <div className="fat-graph" style={mystyle}>
+        <Bar data={fatdata} options={options} />
+      </div>
+      <h1>Other Nutrients</h1>
+      <div className="other-graph" style={otherstyle}>
+        <Bar data={otherdata} options={options} />
+      </div>
+    </div>
+  );
+}
